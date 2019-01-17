317b678713c9e5c91b30cbf7a0fdd26a77d2e8c9
RobinsonCzajkowski <rczajko@rockets.utoledo.edu>
2019-01-17 18:57:33 -0500

    Skeleton Code

--- src/bc19/MyRobot.java
+++ src/bc19/MyRobot.java
@@ -1,60 +1,37 @@
 package bc19;
 
 public class MyRobot extends BCAbstractRobot {
+    int step = -1;
 
     public Action turn() {
-        if (me.unit == SPECS.CASTLE) {
-            log("Building a pilgrim.");
-            return buildUnit(SPECS.PILGRIM,1,0);
-        }
-        return move(1,0);
+        step++;
 
+        if(me.unit == SPECS.CASTLE) {
+
+        }
+
+        else if(me.unit == SPECS.CHURCH) {
+
+        }
+
+        else if(me.unit == SPECS.PILGRIM) {
+
+        }
+
+        else if(me.unit == SPECS.CRUSADER) {
+
+        }
+
+        else if(me.unit == SPECS.PROPHET) {
+
+        }
+
+        else if(me.unit == SPECS.PREACHER) {
+
+        }
+
+        else {
+            log("This unit is not a knows unit");
+        }
     }
 }
-// package bc19;
-// import java.awt.Point;
-
-// public class MyRobot extends BCAbstractRobot {
-// 	public int turn;
-// 	public Point destination;
-
-//     public Action turn() {
-//     	turn++;
-
-//     	if (me.unit == SPECS.CASTLE) {
-//     		if (turn == 1) {
-//     			log("Building a pilgrim.");
-//     			return buildUnit(SPECS.PILGRIM,1,0);
-//     		}
-
-// 			Robot[] visibleRobots = getVisibleRobots();
-// 			for(Robot r: visibleRobots) {
-// 				if (r.team != me.team) {
-// 					int diffX = r.x - me.x;
-// 					int diffY = r.y - me.y;
-// 					return attack(diffX, diffY);
-// 				}
-// 			}
-
-// 			Point myLocation = new Point(me.x, me.y);
-
-// 			if (destination == null) {
-// 				destination = Navigation.reflect(myLocation, getPassableMap(), me.id % 2 == 0);
-// 			}
-
-// 			Point movementDirection = Navigation.goTo(myLocation, destination, getPassableMap(), getVisibleRobotMap());
-// 			return move(movementDirection.x, movementDirection.y);
-//     	}
-
-//     	if (me.unit == SPECS.PILGRIM) {
-//     		if (turn == 1) {
-//     			log("I am a pilgrim.");
-
-//                 //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
-//     		}
-//     	}
-
-//     	return null;
-
-// 	}
-// }
