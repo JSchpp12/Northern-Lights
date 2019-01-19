4cdf9193b3b3851400831a3d82381859fd53f945
RobinsonCzajkowski <rczajko@rockets.utoledo.edu>
2019-01-18 20:54:26 -0500

    This AI can create pilgrims in a circle around the castle Includes a few more helpful methods Can dynamically scan tiles adjacent to units

--- src/bc19/MyRobot.java
+++ src/bc19/MyRobot.java
@@ -1,23 +1,35 @@
 package bc19;
 
-//import java.util.Arrays;
-
 public class MyRobot extends BCAbstractRobot {
     int step = -1;
 
     int tradeKarbon = 0;
     int tradeFuel = 0;
 
+    //Use these as one tile steps in a direction
+    int[] NORTH = {0, -1};
+    int[] NORTHEAST = {1, -1};
+    int[] EAST = {1, 0};
+    int[] SOUTHEAST = {1, 1};
+    int[] SOUTH = {0, 1};
+    int[] SOUTHWEST = {-1, 1};
+    int[] WEST = {-1, 0};
+    int[] NORTHWEST = {-1, -1};
+
+    //Call this array to cycle through all directions easily
+    int[][] myDirections = {NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST};
+
     public Action turn() {
         step++;
 
         if(me.unit == SPECS.CASTLE) {
-            int[][] visibleMap = getVisibleRobotMap();
-            if(isEmpty(1,1) && !isOccupied(1, 1)) {
-                log("Creating a pilgrim to the south east");
-                return buildUnit(SPECS.PILGRIM, 1, 1);
+
+            int pref = findOpenBuildSpot(0);
+            if(pref > -1) {
+                log("Building Unit");
+                return buildUnit(SPECS.PILGRIM, myDirections[pref][0], myDirections[pref][1]);
             }
-            return proposeTrade(tradeKarbon, tradeFuel);
+            return proposeTrade(tradeKarbon, tradeFuel); //If the castle can't find anything to do, it ends its turn by proposing a trade
         }
 
         else if(me.unit == SPECS.CHURCH) {
@@ -41,15 +53,16 @@ public class MyRobot extends BCAbstractRobot {
         }
 
         else {
-            log("This unit is not a knows unit");
+            log("This unit is not a known unit");
         }
     }
 
-    public boolean isEmpty(int dx, int dy) {
-        //log("passsable: " + String.valueOf(map[me.x + dx][me.y + dy]));
-        return map[me.x + dx][me.y + dy];
+    //Checks to see if the tile dx and dy away from me is passable.
+    public boolean isPassable(int dx, int dy) {
+        return map[me.y + dy][me.x + dx];
     }
 
+    //Checks to see if the tile dx and dy away from me is occupied by another robot
     public boolean isOccupied(int dx, int dy) {
         int posX = me.x + dx;
         int posY = me.y + dy;
@@ -57,14 +70,36 @@ public class MyRobot extends BCAbstractRobot {
 
         for(int i = 0; i < robots.length; i++) {
             if(robots[i].x == posX && robots[i].y == posY) {
-                //log("it's occupied");
                 return true;
             }
         }
         return false;
     }
 
-    public Point findOpenBuildSpot() {
+    //Checks to see if the tile is empty (no mountains or robots there)
+    public boolean isEmpty(int dx, int dy) {
+        return isPassable(dx, dy) && !isOccupied(dx, dy);
+    }
+
+    //Cycles through all tiles adjacent to me and returns the direction of an open spot to build
+    //The pref in the preffered direction to build in
+    //The int returned in the myDirections index of the correct direction
+    public int findOpenBuildSpot(int pref) {
+        if(isEmpty(myDirections[pref%myDirections.length][0], myDirections[pref%myDirections.length][1]))
+            return pref%myDirections.length;
+
+        for(int i = 1; i < 5; i++) {
+            if(isEmpty(myDirections[(pref + i)%myDirections.length][0], myDirections[(pref + i)%myDirections.length][1])) //Clockwise
+                return (pref + i)%myDirections.length;
+
+            pref += myDirections.length;
+
+            if(isEmpty(myDirections[(pref - i)%myDirections.length][0], myDirections[(pref - i)%myDirections.length][1])) //Counterclockwise
+                return (pref - i)%myDirections.length;
+            pref -= myDirections.length;
+        }
+        log("No open positions");
+        return -1;
     }
 }
 
